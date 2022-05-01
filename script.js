cd1cb16acb182cbfeb38138025dc4233edd1300b
NoahSaberhagen <noahsaberhagen@gmail.com>
2022-05-01 16:03:24 -0400

    displaying a shopping cart top right its kinda janky but its a start

--- script.js
+++ script.js
@@ -2,10 +2,24 @@
 
 const mainBodyItem = document.querySelectorAll(".main-body__item");
 
+const nav = document.querySelector("nav");
+const cart = document.createElement("ul");
+cart.textContent = "your cart:"
+cart.style.listStyle = "none";
+
+nav.appendChild(cart);
+
 mainBodyItem.forEach((item) => {
     const addToCartButton = document.createElement("button");
-    addToCartButton.textContent = "ewfoi"
+    addToCartButton.textContent = "add to cart"
+    addToCartButton.addEventListener('click', () => {
+        const itemAdded = document.createElement('li');
+        itemAdded.textContent = item.textContent;
+        cart.appendChild(itemAdded)
+    });
     item.appendChild(addToCartButton);
 });
+
+
 
 // end shopping cart -------------------------------------------------------------------------
