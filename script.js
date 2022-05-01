deac768246b3b273c147523a588ef3c924428ea4
NoahSaberhagen <noahsaberhagen@gmail.com>
2022-05-01 15:13:26 -0400

    initial

---
+++ script.js
@@ -0,0 +1,12 @@
+// begin shopping cart ----------------------------------------------------------------------
+
+const mainBody = document.querySelector('.main-body');
+
+const mainBodyBoxes = mainBody.querySelector('p');
+const mainBodyBoxesNum = mainBody.querySelector('p').value;
+
+alert(mainBodyBoxesNum);
+
+
+
+// end shopping cart -------------------------------------------------------------------------
