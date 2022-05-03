724521d673cdf2ab37748517c738c652be51a6c5
Isaac Rodriguez <isaac.bh.rod@gmail.com>
2022-05-03 13:50:26 -0400

    lefts note, for loop too add buttons to each div WITHOUT using HTML

--- script.js
+++ script.js
@@ -1,23 +1,38 @@
-const div = document.querySelector(".box");
+const newBtn = () => {
+    const  div = document.querySelector(".box");
+    const addToCart = document.createElement("button");
+    addToCart.innerText = "Add To Cart";
+    addToCart.classList.add("updateCart");
+    div.appendChild(addToCart);
+   }
 
+   const div = document.querySelector(".box");
+   
 const newPlusBtn = document.createElement("button");
 newPlusBtn.innerText = "+";
 newPlusBtn.classList.add("plus");
 div.appendChild(newPlusBtn);
+
+newBtn() 
 
 const newMinusBtn = document.createElement("button");
 newMinusBtn.innerText = "-";
 newMinusBtn.classList.add("minus");
 div.appendChild(newMinusBtn);
 
-const newBtn = () => {
- const  div = document.querySelector(".box");
- const addToCart = document.createElement("button");
- addToCart.innerText = "Add To Cart";
- addToCart.classList.add("updateCart");
- div.appendChild(addToCart);
+/*const productBox = document.querySelectorAll(".box");
+
+for (let i = 0; i < productBox.length; i++) {
+  const productBoxs = document.querySelector(".box");
+  const newPlusBtn = document.createElement("button");
+  newPlusBtn.innerText = "+";
+  newPlusBtn.classList.add("plus");
+  productBoxs.appendChild(newPlusBtn);
 }
- newBtn() 
+console.log(productBox) */
+
+
+
 
 
 
