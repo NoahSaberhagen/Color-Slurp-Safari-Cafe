10ad6c7bdc44eb589b8064b4afddda923f033a23
Isaac Rodriguez <isaac.bh.rod@gmail.com>
2022-05-06 18:52:26 -0400

    updated script.JS file, left comments on trying to figure out a for loop

--- script.js
+++ script.js
@@ -1,23 +1,45 @@
 const div = document.querySelector(".box");
+const newBtn = () => {
+  const addToCart = document.createElement("button");
+  addToCart.innerText = "Add To Cart";
+  addToCart.classList.add("updateCart");
+  div.appendChild(addToCart);
+};
 
+const plusBtn = () => {
 const newPlusBtn = document.createElement("button");
 newPlusBtn.innerText = "+";
 newPlusBtn.classList.add("plus");
 div.appendChild(newPlusBtn);
+};
 
+const minusBtn = () => {
 const newMinusBtn = document.createElement("button");
 newMinusBtn.innerText = "-";
 newMinusBtn.classList.add("minus");
 div.appendChild(newMinusBtn);
+};
 
-const newBtn = () => {
- const  div = document.querySelector(".box");
- const addToCart = document.createElement("button");
- addToCart.innerText = "Add To Cart";
- addToCart.classList.add("updateCart");
- div.appendChild(addToCart);
+plusBtn();
+newBtn();
+minusBtn(); 
+
+/* const shopBtns = document.querySelectorAll(".box")
+for(let i = 0; i < shopBtns.length; i++) {
+  plusBtn();
+  newBtn();
+  minusBtn();
+}   I FELL LIKE IM SO CLOSE */
+
+/*const productBox = document.querySelectorAll(".box");
+
+for (let i = 0; i < productBox.length; i++) {
+  const productBoxs = document.querySelector(".box");
+  const newPlusBtn = document.createElement("button");
+  newPlusBtn.innerText = "+";
+  newPlusBtn.classList.add("plus");
+  productBoxs.appendChild(newPlusBtn);
 }
- newBtn() 
-
+console.log(productBox)*/
 
 
